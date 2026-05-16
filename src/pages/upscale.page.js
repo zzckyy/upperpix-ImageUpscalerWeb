f8301250676d48d446b2aae2f5c30e1400bb468f
zzckyy <zackymm2080@gmail.com>
2026-05-16 21:28:32 +0700

    feat: upscaling image now is working(no worker yet)

--- src/pages/upscale.page.js
+++ src/pages/upscale.page.js
@@ -1,9 +1,17 @@
-// import Upscale from 'upscaler'
+
 import "../style.css";
 import javascriptLogo from "../assets/javascript.svg";
 import viteLogo from "../assets/vite.svg";
 import heroImg from "../assets/hero.png";
 import "milligram";
+
+import * as tf from "@tensorflow/tfjs";
+import Upscaler from "upscaler";
+// import Upscaler from "upscaler/dist/browser/esm";
+
+await tf.setBackend('webgl');
+await tf.ready();
+const upscaler = new Upscaler();
 
 const brand = "Upperpix";
 const hero = "Make your photo more live with";
@@ -39,30 +47,65 @@ document.querySelector("#app").innerHTML = /*html */ `
             
             <div class="output-container">
                 <p>UPSCALED</p>
-                <img src="" class="image-output" />
-                Download image
+                <div class="" id="image-output"></div>
+                <a id="download"><button>Download Image</button></a>
             </div>
         </div>
 
     </section>
 `
 
+let currentURL;
 const input = document.querySelector('#image-input');
-const label = document.querySelector('#label-input')
+const label = document.querySelector('#label-input');
+const output = document.querySelector('#image-output');
+const downloadBtn = document.querySelector('#download');
 
-input.addEventListener('change', (e) =>{
-    const files = e.target.files;
-    const file = files[0]
-    input.style.backgroundImage = "url(files[0])"
+input.addEventListener('change', async (e) => {
+    const file = e.target.files[0];
+    const image = new Image();
+
+    if (!file) {
+        label.textContent = "Upload your image here"
+        downloadBtn.style.color = "none";
+        return;
+    }
+
+    if (currentURL) { URL.revokeObjectURL(currentURL) };
 
     const url = URL.createObjectURL(file);
+    currentURL = url
+
     input.style.backgroundImage = `url(${url})`;
+    label.textContent = file.name
+
+    image.src = url;
+    output.innerHTML = "Processing...";
+    // kasih napas ke browser
+    await new Promise(r => setTimeout(r, 50));
+
+    // resize dulu
+    // const MAX_SIZE = 1024;
+    await image.decode();
+
     
-    if(files.length > 0){
-        label.textContent = `${file.name}`
+
+    // upscale
+    const result = await upscaler.upscale(image, {
+        scale: 2,
+        patchSize: 32,
+        padding: 2
+    });
+
+    output.innerHTML = "";
+    output.innerHTML = `<img src="${result}" alt="Upscaled Image" class="image-output"/>`;
+
+    downloadBtn.onclick = () => {
+        if (!result) { alert("blom ada gambar") }
+        downloadBtn.href = result;
+        downloadBtn.download = "upscaled.png";
+
     }
 
-    else{
-        label.textContent = "Upload your image here"
-    }
 })
+
